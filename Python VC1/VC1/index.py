a81eeac456716a792efe82d8ad51aeaca56bfa92
Lyhor-Ngorn <lyhor.ngorn@student.passerellesnumeriques.org>
2021-02-26 21:24:29 +0700

    Add background,ship'customer,Bullet

--- Python VC1/VC1/index.py
+++ Python VC1/VC1/index.py
@@ -1,16 +1,11 @@
 from tkinter import *
 root=Tk()
-root.geometry('1200x600')
+root.geometry('1500x900')
 root.resizable(False,False)
 root.title('Hello the best team')
 canvas=Canvas()
-canvas.configure(bg='black')
-
-
-
-
-
-
+background=PhotoImage(file='VC1/BG.png')
+canvas.create_image(750,400,image=background)
 
 
 #The ship movement
@@ -18,41 +13,51 @@ def moveLeft(event):
     global ourShip
     x,y=canvas.coords(ourShip)
     if x>70:
-        canvas.move(ourShip,-10,0)
-        canvas.move(shooting1,-10,0)
-        canvas.move(shooting2,-10,0)
+        canvas.move(ourShip,-40,0)
+
 def moveRight(event):
     global ourShip
     x,y=canvas.coords(ourShip)
-    if x<1200:
-        canvas.move(ourShip,10,0)
-        canvas.move(shooting2,10,0)
-        canvas.move(shooting1,10,0)
+    if x<1500:
+        canvas.move(ourShip,40,0)
+
 def moveUp(event):
     global ourShip
     x,y=canvas.coords(ourShip)
-    if y>0 and y<=600:
-        canvas.move(ourShip,0,-10)
-        canvas.move(shooting1,0,-10)
-        canvas.move(shooting2,0,-10)
+    if y>0:
+        canvas.move(ourShip,0,-40)
+
 def moveDown(event):
     global ourShip
     x,y=canvas.coords(ourShip)
-    if y>=0 and y<=520:
-        canvas.move(ourShip,0,10)
-        canvas.move(shooting2,0,10)
-        canvas.move(shooting1,0,10)
-def shooting(event):
-    global shooting1,shooting2
-    x3,y3=canvas.coords(shooting2)
-    canvas.move(shooting1,0,-10)
-    canvas.move(shooting2,0,-10)
-    canvas.after(10,lambda:shooting(event))
+    if y<800:
+        canvas.move(ourShip,0,40)
 
 
+def shoootingIt():
+    canvas.move(shooting1,0,-20)
+    canvas.move(shooting2,0,-20)
 
+    y=canvas.coords(shooting1)[1]
+    stopShooting = y <=0
 
+    if not stopShooting:
+        canvas.after(5,lambda:shoootingIt())
+    else :
+        canvas.delete(shooting1)
+        canvas.delete(shooting2)
+        shooting()
+   
 
+def shooting():
+    global ourShip,shooting1,shooting2, bullet,alwayShoot,y1
+
+    x1 = canvas.coords(ourShip)[0]
+    y1 = canvas.coords(ourShip)[1]
+    
+    shooting1=canvas.create_image(x1-60,y1+35,image=bullet)
+    shooting2=canvas.create_image(x1-2,y1+35,image=bullet)
+    shoootingIt()
 
 
 
@@ -60,15 +65,18 @@ def shooting(event):
 
 
 #our ship
+
+
+
+
+
 bullet=PhotoImage(file='VC1/bullet_resize.png')
-shooting1=canvas.create_image(545,430,image=bullet,anchor=NE)
-shooting2=canvas.create_image(505,430,image=bullet,anchor=NE)
 ship=PhotoImage(file = "VC1/spaceship.png", name = "mouse_pointer",)
-ourShip=canvas.create_image(550,400,image=ship,anchor=NE)
+ourShip=canvas.create_image(750,750,image=ship,anchor=NE)
 
 
 
-
+#enermy
 
 
 
@@ -76,11 +84,13 @@ ourShip=canvas.create_image(550,400,image=ship,anchor=NE)
 
 
 #action of the ship 
-root.bind('<Left>',moveLeft)
-root.bind('<Right>',moveRight)
-root.bind('<Up>',moveUp)
-root.bind('<Down>',moveDown)
-root.bind('<Button-1>',shooting)
+root.bind('<a>',moveLeft)
+root.bind('<d>',moveRight)
+root.bind('<w>',moveUp)
+root.bind('<s>',moveDown)
+
+
+shooting()
 
 
 
