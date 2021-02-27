97f0cffb01e9d9601080f1d1223d0305890405b3
Lyhor-Ngorn <lyhor.ngorn@student.passerellesnumeriques.org>
2021-02-27 16:05:40 +0700

    Try to add sound but not yet done

--- Python VC1/VC1/index.py
+++ Python VC1/VC1/index.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from pygame import mixer
+from playsound import playsound
 root=Tk()
 root.geometry('1500x900')
 root.resizable(False,False)
@@ -6,7 +8,7 @@ root.title('Hello the best team')
 canvas=Canvas()
 background=PhotoImage(file='VC1/BG.png')
 canvas.create_image(750,400,image=background)
-
+soundObj = mixer.Sound('VC1/laser.wav')
 
 #The ship movement
 def moveLeft(event):
@@ -37,12 +39,11 @@ def moveDown(event):
 def shoootingIt():
     canvas.move(shooting1,0,-20)
     canvas.move(shooting2,0,-20)
-
     y=canvas.coords(shooting1)[1]
     stopShooting = y <=0
 
     if not stopShooting:
-        canvas.after(5,lambda:shoootingIt())
+        canvas.after(10,lambda:shoootingIt())
     else :
         canvas.delete(shooting1)
         canvas.delete(shooting2)
@@ -50,14 +51,40 @@ def shoootingIt():
    
 
 def shooting():
-    global ourShip,shooting1,shooting2, bullet,alwayShoot,y1
-
+    global ourShip,shooting1,shooting2, bullet,y1
     x1 = canvas.coords(ourShip)[0]
     y1 = canvas.coords(ourShip)[1]
-    
     shooting1=canvas.create_image(x1-60,y1+35,image=bullet)
     shooting2=canvas.create_image(x1-2,y1+35,image=bullet)
+    ('VC1/laser.wav')
     shoootingIt()
+def e_move(event):
+    global enermy,eachEnermy,x,y
+    x1=canvas.coords(eachEnermy)[0]
+    y1=canvas.coords(eachEnermy)[1]
+    if x1==1500:
+        x=-x
+
+    elif x1==0:
+        x=-x
+    canvas.move(eachEnermy,x,y)
+    if y1>=900:
+        canvas.delete(eachEnermy)
+        canvas.after(10,lambda:createEnermy(event))
+    else:
+        canvas.after(10,lambda:e_move(event))
+def createEnermy(event):
+    global enermy,eachEnermy
+
+    eachEnermy=canvas.create_image(200,0,image=e_ship)
+    e_move(event)
+def startEnermy(event):
+    global text,enermy
+    canvas.delete(text)
+    createEnermy(event)
+
+
+
 
 
 
@@ -77,9 +104,10 @@ ourShip=canvas.create_image(750,750,image=ship,anchor=NE)
 
 
 #enermy
+e_ship=PhotoImage(file='VC1/enermy.png')
 
-
-
+x=5
+y=3
 
 
 
@@ -88,6 +116,12 @@ root.bind('<a>',moveLeft)
 root.bind('<d>',moveRight)
 root.bind('<w>',moveUp)
 root.bind('<s>',moveDown)
+root.bind('<space>',startEnermy)
+
+text=canvas.create_text(900,450,fill="white",font="Times 40 italic bold",text="Space to start game")
+
+
+
 
 
 shooting()
